484301211e301b934b23eaeeef4c952c9866323a
Zijin Xiao <28256042@qq.com>
2016-11-19 09:56:16 +0800

    remove jshint

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,14 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
 import navbar from './components/navbar.vue';
-import footer from './components/cfooter.vue';
+import cfooter from './components/cfooter.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(ElementUI)
 
 Vue.component('navbar',navbar);
-Vue.component('cfooter',footer);
+Vue.component('cfooter',cfooter);
 
 var router = new VueRouter({
   routes: require('./routes')
